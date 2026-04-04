d00c4f16373432796f8c46e1d6778c9231ba731d
Tae Young <49816869+teon98@users.noreply.github.com>
2026-04-04 15:10:18 +0900

    발등의 불 🔥

---
+++ 2026-04/선택정렬.py
@@ -0,0 +1,15 @@
+"""
+선택 정렬
+- 가장 작은 데이터를 선택해 맨 앞에 있는 데이터와 바꾸고, 그 다음 작은 데이터를 선택해 앞에서 두 번째 데이터와 바꾸는 과정 반복
+- 시간 복잡도 O(N²)
+"""
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(array)):
+    min_index = i #가장 작은 원소의 인덱스
+    for j in range(i+1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
+
+print(array)
---
+++ 2026-04/순차탐색.py
@@ -0,0 +1,10 @@
+"""
+순차 탐색
+- 리스트 안에 있는 특정한 데이터를 찾기 위해 앞에서부터 데이터를 하나씩 차례대로 확인하는 방법
+"""
+def sequential_search(n, target, array):
+    #각 원소를 하나씩 확인하며
+    for i in range(n):
+        #현재의 원소가 찾고자 하는 원소와 동일한 경우
+        if array[i] == target:
+            return i + 1 # 현재의 위치 반환(인덱스는 0부터 시작하므로 1더하기)
